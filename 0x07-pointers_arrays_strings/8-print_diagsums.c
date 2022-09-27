3f5e985b6a25ae1ce1edf5fdda9e25fbce2ec403
Barnabas <techybanner@gmail.com>
2022-09-27 16:24:03 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - print additions of the two diagonals
- * @a: source matrix
- * @size: size NxN of matrix
- *
- * Return: chessboard.
+ * print_diagsums - Prints the sums of the two diagonals
+ * @a: The matrix of integers.
+ * @size: The size of the matrix.
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sumdiag1 = 0, sumdiag2 = 0, step1, step2;
+	int index, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size - 1); i++)
+	for (index = 0; index < size; index++)
 	{
-		step1 = (size + 1) * 1;
-		sumdiag1 = sumdiag1 + *(a + step1);
+		sum1 += a[index];
+		a += size;
 	}
 
-	for (j = 1; j <= size; j++)
+	a -= size;
+
+	for (index = 0; index < size; index++)
 	{
-		step2 = (size - 1) * j;
-		sumdiag2 = sumdiag2 + *(a + step2);
+		sum2 += a[index];
+		a -= size;
 	}
-	printf("%d, %d\n", sumdiag1, sumdiag2);
 }
